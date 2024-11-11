6a50d5f0e8a9216fee43ef4aed06bb4cf56b7ba1
Spencer Anderson <lastof6.boyz@gmail.com>
2024-11-11 15:08:44 -0700

    Main Menu moved to be inside of the main level scene

--- ImmortalScrewdriver/Assets/Scripts/TextResponderMainMenu.cs
+++ ImmortalScrewdriver/Assets/Scripts/TextResponderMainMenu.cs
@@ -1,12 +1,13 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class TextResponderMainMenu : MonoBehaviour
 {
     public TextMeshProUGUI inputTextField;  // Reference to the input text field
     public TextMeshProUGUI outputTextField; // Reference to the output text field
+    public Transform playerTransform;       // Reference to the player's Transform
+    public GameObject targetObject;         // Reference to the target GameObject
 
     // Call this method, e.g., on a button click to evaluate the input text
     public void RespondToInput()
@@ -17,9 +18,9 @@ public class TextResponderMainMenu : MonoBehaviour
         switch (inputText)
         {
             case "start":
-                outputTextField.text = "C:Users/Owner>START \n" +
+                outputTextField.text = "C:/Users/Owner>START \n" +
                     "Initializing experiment, please wait...";
-                StartCoroutine(LoadSceneAfterDelay(3f, "MainScene")); // Replace "YourSceneName" with the actual scene name
+                StartCoroutine(MovePlayerAfterDelay(3f)); // Move the player instead of loading a scene
                 break;
 
             default:
@@ -32,9 +33,17 @@ public class TextResponderMainMenu : MonoBehaviour
         inputTextField.text = "";
     }
 
-    private IEnumerator LoadSceneAfterDelay(float delay, string sceneName)
+    private IEnumerator MovePlayerAfterDelay(float delay)
     {
         yield return new WaitForSeconds(delay);
-        SceneManager.LoadScene(sceneName);
+        if (playerTransform != null && targetObject != null)
+        {
+            playerTransform.position = targetObject.transform.position;      // Set the player's position to the target's position
+            playerTransform.rotation = targetObject.transform.rotation;      // Set the player's rotation to the target's rotation
+        }
+        else
+        {
+            Debug.LogWarning("Player Transform or Target Object is not assigned.");
+        }
     }
 }
